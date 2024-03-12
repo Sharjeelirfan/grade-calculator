afef9b2497b36300552d4a65e3f1fdb4b8ac2474
sharjeel <sharjeelkhatri303@gmail.com>
2024-03-12 23:34:14 +0500

    grade-claculator

--- app.js
+++ app.js
@@ -1,7 +1,23 @@
 function cal(){
     let total = document.getElementById("total").value;
     let obtained = document.getElementById("obtained").value;
-    let sum = obtained / total  * 100
-    document.getElementById("ans").innerHTML = `your percentage is ${sum}%`
+    let sum = obtained / total  * 100;
+
+    let grade = ""
+
+    if(sum >= 80){
+        grade = 'A+'
+    }else if(sum >=70){
+        grade = 'A'
+    }else if(sum >= 60){
+        grade = 'B'
+    }else if(sum >= 50){
+        grade = 'C'
+    }else if(sum >= 40){
+        grade = 'D'
+    }else if(sum >= 0){
+        grade = 'Fail'
+    }
+    document.getElementById("ans").innerHTML = "your grade is " + grade
 
 }
